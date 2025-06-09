d8aee9bfff22a931851731abe8a7a39573719984
Niranjan Achar <niranjan.achar@publicissapient.com>
2025-06-09 11:33:29 +0530

    Remove Name tag from aws_instance resource in main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 resource "aws_instance" "web" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t3.micro"
-
-  tags = {
-    Name = "WebServer"
-  }
 }
